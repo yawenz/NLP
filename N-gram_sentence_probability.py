3435b5c213082b14ef34f35325ac52cf4cd2f866
Yawen Zhang <Yawen.Zhang@colorado.edu>
2017-03-08 18:58:47 -0700

    Add files via upload

--- N-gram_sentence_probability.py
+++ N-gram_sentence_probability.py
@@ -18,8 +18,8 @@ class NgramCount:
         ngram = {}
         for x in data:
             x = x.split(" ")
-            for i in range(len(x)-n+1):
-                grams = ' '.join(x[i:i+n])
+            for i in range(len(x) - n + 1):
+                grams = ' '.join(x[i:i + n])
                 ngram.setdefault(grams, 0)
                 ngram[grams] += 1
         return ngram
@@ -29,7 +29,7 @@ class NgramProb:
     Class to calcualte N-gram probability
     """
     def GetUnigramProbabilities(self, unigram_data):
-        # count the total instances of tokens, get rid of start and ending tokens
+        # count the total instances of tokens, not include start and end tokens
         total_count = sum(unigram_data.values()) - unigram_data["<s>"] - unigram_data["</s>"]
         # define the unigram prob.
         unigram_prob = {}
@@ -50,24 +50,28 @@ class NgramProb:
     def GetSmoothedProbabilities(self, unigram_data, bigram_data):
         # define k
         k = 0.0001
-
-        # define the smoothed bigram prob.
+        # define the smoothed bigram prob. 
+        # recalculate prob. smoothed_bigram_prob: for non zero prob, smoothed_bigram_prob_zero: for zero prob. 
         smoothed_bigram_prob = {}
+        smoothed_bigram_prob_zero = {}
         # get smoothed bigram probability
         for key, value in bigram_data.items():
             key_unigram = key.split(" ")[0]
-            smoothed_bigram_prob[key] = math.log10((value + k)/(unigram_data[key_unigram] + k*(len(unigram_data)-2)))
-        
-        return smoothed_bigram_prob
+            smoothed_bigram_prob[key] = math.log10((value + k)/(unigram_data[key_unigram] + k*(len(unigram_data))))
+        for key, value in unigram_data.items():
+            smoothed_bigram_prob_zero[key] = math.log10(k/(value + k*(len(unigram_data))))       
+        return smoothed_bigram_prob, smoothed_bigram_prob_zero
 
 class NgramSentenceProb:
     """
     Class to estimate sentence N-gram probability
     """
     def GetUnigramSentenceProbability(self, sentence, unigram_prob):
+        # get tokens from sentence
         sentence = sentence.replace("\n", "")
         tokens = sentence.split(" ")
-
+        
+        # calculate unigram prob. 
         unigram_sentence_prob = 0
         for x in tokens:
             if x in unigram_prob.keys():
@@ -77,7 +81,7 @@ class NgramSentenceProb:
                 break            
         return unigram_sentence_prob
 
-    def GetBigramSentenceProbability(self, sentence, unigram_prob, bigram_prob):
+    def GetBigramSentenceProbability(self, sentence, bigram_prob):
         # add start and ending tokens to sentence
         sentence = "<s> " + sentence.replace("\n", " </s>")
         tokens = sentence.split(" ")
@@ -88,7 +92,7 @@ class NgramSentenceProb:
             g = ' '.join(tokens[i:i+2])
             sentence_bigram.append(g)
         
-        # estimate sentence bigram probability using chain rule
+        # calculate sentence bigram probability using chain rule
         bigram_sentence_prob = 0
         for j in sentence_bigram:
             if j in bigram_prob.keys():
@@ -99,8 +103,7 @@ class NgramSentenceProb:
                 break
         return bigram_sentence_prob
 
-    def GetSmoothedSentenceProbability(self, sentence, unigram_data, unigram_prob, smoothed_bigram_prob):
-        k = 0.0001
+    def GetSmoothedSentenceProbability(self, sentence, smoothed_bigram_prob, smoothed_bigram_prob_zero):
         # add start and ending tokens to sentence
         sentence = "<s> " + sentence.replace("\n", " </s>")
         tokens = sentence.split(" ")
@@ -111,15 +114,14 @@ class NgramSentenceProb:
             g = ' '.join(tokens[i:i+2])
             smoothed_sentence_bigram.append(g)
         
-        # estimate sentence smoothed bigram probability using chain rule
+        # calculate sentence smoothed bigram probability using chain rule
         smoothed_bigram_sentence_prob = 0
         for j in smoothed_sentence_bigram:
             if j in smoothed_bigram_prob.keys():
                 smoothed_bigram_sentence_prob += smoothed_bigram_prob[j]
             else:
-                # if not in the bigram prob. model, calculate the smoothed bigram prob. 
-                j_unigram = j.split(" ")[0]
-                smoothed_bigram_sentence_prob += math.log10(k/(unigram_data[j_unigram] + k*(len(unigram_data)-2)))
+                # if not in the bigram prob. model, use the smoothed prob.
+                smoothed_bigram_sentence_prob += smoothed_bigram_prob_zero[j.split(" ")[0]]
         return smoothed_bigram_sentence_prob
 
 if __name__ == "__main__":
@@ -145,7 +147,7 @@ if __name__ == "__main__":
     ngram_prob = NgramProb()
     unigram_prob = ngram_prob.GetUnigramProbabilities(unigram_count)
     bigram_prob = ngram_prob.GetBigramProbabilities(unigram_count, bigram_count)
-    smoothed_bigram_prob = ngram_prob.GetSmoothedProbabilities(unigram_count, bigram_count)
+    smoothed_bigram_prob, smoothed_bigram_prob_zero = ngram_prob.GetSmoothedProbabilities(unigram_count, bigram_count)
 
     # read the testing data
     test_file = argv[2]
@@ -153,7 +155,7 @@ if __name__ == "__main__":
     with open(test_file, 'r') as f:
     	test_data = f.readlines()
 
-    # estimate sentence N-gram probability for test data
+    # calculate sentence N-gram probability for test data
     for sentence in test_data:
         print("S = ", sentence)
         
@@ -163,19 +165,19 @@ if __name__ == "__main__":
         ngram_sentence_prob = NgramSentenceProb()
         unigram_sentence_prob = ngram_sentence_prob.GetUnigramSentenceProbability(sentence, unigram_prob)
         if unigram_sentence_prob != 0:
-            print("Unigrams: logprob(S) = ", round(unigram_sentence_prob, 4))
+            print("Unigrams: logprob(S) = ", "%.4f" % unigram_sentence_prob)
         else:
             # if prob equals to 0
             print("Unigrams: logprob(S) = undfined")
-        bigram_sentence_prob = ngram_sentence_prob.GetBigramSentenceProbability(sentence, unigram_prob, bigram_prob)
+        bigram_sentence_prob = ngram_sentence_prob.GetBigramSentenceProbability(sentence, bigram_prob)
         if bigram_sentence_prob != 0:
-            print("Bigrams: logprob(S) = ", round(bigram_sentence_prob, 4))
+            print("Bigrams: logprob(S) = ", "%.4f" % bigram_sentence_prob)
         else:
             # if prob equals to 0
             print("Bigrams: logprob(S) = undefined")
-        smoothed_bigram_sentence_prob = ngram_sentence_prob.GetSmoothedSentenceProbability(sentence, unigram_count, unigram_prob, smoothed_bigram_prob)
+        smoothed_bigram_sentence_prob = ngram_sentence_prob.GetSmoothedSentenceProbability(sentence, smoothed_bigram_prob, smoothed_bigram_prob_zero)
         if smoothed_bigram_sentence_prob != 0:
-            print("Smoothed Bigrams: logprob(S) = ", round(smoothed_bigram_sentence_prob, 4))
+            print("Smoothed Bigrams: logprob(S) = ", "%.4f" % smoothed_bigram_sentence_prob)
         else:
             # if prob equals to 0
             print("Smoothed Bigrams: logprob(S) = undefined")
